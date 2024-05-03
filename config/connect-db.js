e99f13848267d5cdc2c48865d9c4f92f07f8501d
nikunj71 <nikunj.dhaduk7@gmail.com>
2024-05-03 14:07:59 +0530

    add new db url

--- config/connect-db.js
+++ config/connect-db.js
@@ -3,10 +3,13 @@ mongoose.Promise = global.Promise;
 console.log('process.env.MONGO_URL', process.env.MONGO_URL);
 (async function () {
   try {
-    await mongoose.connect('mongodb://localhost:27017/rating', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      'mongodb+srv://nikunj71:nikunj71@cluster0.kddsfwl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     // await mongoose.connect("mongodb://localhost:27017/joypabe", {
     //   useNewUrlParser: true,
     //   useUnifiedTopology: true,
